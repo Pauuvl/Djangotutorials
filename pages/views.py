f513aeb2fc91131946e72c80d44d28432d59c8d5
Pauuvl <159390787+Pauuvl@users.noreply.github.com>
2026-02-22 18:06:09 -0500

    Tutorial Django models, relationships, forms and views completed

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, ListView
 from django.views import View
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django import forms
-from django.core.exceptions import ValidationError  # <-- Agregar esta línea
+from django.core.exceptions import ValidationError
+from .models import Product
+
 
 # ---------- PAGES ----------
 
@@ -15,78 +17,77 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            "title": "About us - Online Store",
-            "subtitle": "About us",
-            "description": "This is an about page",
-            "author": "Developed by: Paulina Velásquez",
-        })
-        return context
-
 
 class ContactPageView(TemplateView):
     template_name = 'pages/contact.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            "title": "Contact - Online Store",
-            "email": "onlinestore@gmail.com",
-            "phone": "+57 234567890",
-            "address": "123 calle 34, Medellín",
-        })
-        return context
-
 
 # ---------- PRODUCTS ----------
-
-class Product:
-    products = [
-        {"id": "1", "name": "TV", "description": "Best TV", "price": 120},
-        {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 999},
-        {"id": "3", "name": "Chromecast", "description": "Best Chromecast", "price": 80},
-        {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 60},
-    ]
-
 
 class ProductIndexView(View):
     template_name = 'products/index.html'
 
     def get(self, request):
-        return render(request, self.template_name, {
-            "title": "Products - Online Store",
-            "subtitle": "List of products",
-            "products": Product.products
-        })
+
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+
+        # 🔥 CAMBIO IMPORTANTE: ahora consulta la BD
+        viewData["products"] = Product.objects.all()
+
+        return render(request, self.template_name, viewData)
 
 
 class ProductShowView(View):
     template_name = 'products/show.html'
 
     def get(self, request, id):
+
         try:
-            product = Product.products[id - 1]
-        except (IndexError, ValueError):
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError("Product id must be 1 or greater")
+
+            product = get_object_or_404(Product, pk=product_id)
+
+        except (ValueError, IndexError):
             return HttpResponseRedirect(reverse('home'))
 
-        return render(request, self.template_name, {
-            "title": product["name"],
-            "product": product
-        })
+        viewData = {}
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " - Product information"
+        viewData["product"] = product
+
+        return render(request, self.template_name, viewData)
+
+
+# ---------- LISTVIEW (opcional del tutorial) ----------
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/index.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Products - Online Store'
+        context['subtitle'] = 'List of products'
+        return context
 
 
 # ---------- FORM ----------
 
-class ProductForm(forms.Form):
-    name = forms.CharField(required=True)
-    price = forms.FloatField(required=True)
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
-        if price <= 0:
-            raise ValidationError("El precio debe ser mayor a cero.")
+        if price is not None and price <= 0:
+            raise ValidationError('Price must be greater than zero.')
         return price
 
 
@@ -95,24 +96,19 @@ class ProductCreateView(View):
 
     def get(self, request):
         form = ProductForm()
-        return render(request, self.template_name, {
-            "title": "Create product",
-            "form": form
-        })
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
 
     def post(self, request):
         form = ProductForm(request.POST)
 
         if form.is_valid():
-            Product.products.append({
-                "id": str(len(Product.products) + 1),
-                "name": form.cleaned_data["name"],
-                "description": "New product",
-                "price": form.cleaned_data["price"],
-            })
-            return HttpResponseRedirect(reverse('products'))
+            form.save()
+            return redirect('products')
 
-        return render(request, self.template_name, {
-            "title": "Create product",
-            "form": form
-        })
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
